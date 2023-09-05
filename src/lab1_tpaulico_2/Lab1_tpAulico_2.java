2d2957dd40b1a494a56cdb1dfa993221984c274e
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-09-05 11:26:09 -0300

    Initial commit

---
+++ src/lab1_tpaulico_2/Lab1_tpAulico_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1_tpaulico_2;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Lab1_tpAulico_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
